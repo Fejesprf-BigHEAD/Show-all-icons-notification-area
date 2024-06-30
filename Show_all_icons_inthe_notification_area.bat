a3aacec91b18804b1910028979007dc2c2b87170
Fejesprf <72359148+Fejesprf-BigHEAD@users.noreply.github.com>
2024-06-30 13:55:23 +0200

    Update Show_all_icons_inthe_notification_area.bat

--- Show_all_icons_inthe_notification_area.bat
+++ Show_all_icons_inthe_notification_area.bat
@@ -2,11 +2,17 @@
 
 REM Set the script path
 set "SCRIPT_DIR=%USERPROFILE%\Scripts"
-if not exist "%SCRIPT_DIR%" mkdir "%SCRIPT_DIR%"
 set "SCRIPT_PATH=%SCRIPT_DIR%\update-notifyicons.ps1"
 
-REM Create the script file
-powershell -executionpolicy bypass -Command "Set-Content -Path '%SCRIPT_PATH%' -Value 'Get-ChildItem \"HKCU:\Control Panel\NotifyIconSettings\" | ForEach-Object { Set-ItemProperty -Path $_.PSPath -Name \"IsPromoted\" -Type DWORD -Value 1 }' -Force"
+REM Check and create the script directory if it doesn't exist
+if not exist "%SCRIPT_DIR%" (
+    mkdir "%SCRIPT_DIR%"
+)
+
+REM Create or overwrite the PowerShell script file
+(
+    echo Get-ChildItem "HKCU:\Control Panel\NotifyIconSettings" ^| ForEach-Object { Set-ItemProperty -Path $_.PSPath -Name "IsPromoted" -Type DWORD -Value 1 }
+) > "%SCRIPT_PATH%"
 
 REM Create the shortcut if it doesn't exist
 set "SHORTCUT_PATH=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\update-notifyicons.lnk"
@@ -15,7 +21,7 @@ set "SHORTCUT_ARGUMENTS=-ExecutionPolicy Bypass -File \"%SCRIPT_PATH%\""
 set "SHORTCUT_DESCRIPTION=Show all icons in the notification area"
 
 if not exist "%SHORTCUT_PATH%" (
-    powershell -executionpolicy bypass -Command "& {$Shell = New-Object -ComObject WScript.Shell; $Shortcut = $Shell.CreateShortcut('%SHORTCUT_PATH%'); $Shortcut.TargetPath = '%SHORTCUT_TARGET%'; $Shortcut.Arguments = '%SHORTCUT_ARGUMENTS%'; $Shortcut.Description = '%SHORTCUT_DESCRIPTION%'; $Shortcut.Save()}"
+    powershell -NoProfile -ExecutionPolicy Bypass -Command "& {$Shell = New-Object -ComObject WScript.Shell; $Shortcut = $Shell.CreateShortcut('%SHORTCUT_PATH%'); $Shortcut.TargetPath = '%SHORTCUT_TARGET%'; $Shortcut.Arguments = '%SHORTCUT_ARGUMENTS%'; $Shortcut.Description = '%SHORTCUT_DESCRIPTION%'; $Shortcut.Save()}"
 )
 
-exit :: Script by BigHEAD
+exit /b
